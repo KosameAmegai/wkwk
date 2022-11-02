533f29ea4a0c2f0228dc9f579c0d8d65804cc544
KosameAmegai <84204311+KosameAmegai@users.noreply.github.com>
2022-11-02 18:44:50 +0700

    Add files via upload

---
+++ r00t.php
@@ -0,0 +1,70 @@
+<!--------------------------------------------
+Hacked by KosameAmegai
+t.me/kosameamegai, Japan
+zone-h.org/archive/notifier=KosameAmegai
+2k19 - Now
+--------------------------------------------->
+<title>Hacked by KosameAmegai</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta property="og:title" content="Hacked by KosameAmegai">
+<meta name="description" content="BABYMETAL - CATCH ME IF YOU CAN">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta property="og:description" content="BABYMETAL - CATCH ME IF YOU CAN">
+<meta property="og:image" content="https://c.top4top.io/p_2492nzc8p1.jpg">
+<link rel="SHORTCUT ICON" href="http://www.pinclipart.com/picdir/big/363-3638329_japanese-flag-clipart.png">
+ <head>
+<style type="text/css">* {cursor: url(http://cur.cursors-4u.net/anime/ani-12/ani1113.ani), url(http://cur.cursors-4u.net/anime/ani-12/ani1113.png), auto !important;}</style><a href="https://www.cursors-4u.com/cursor/2011/03/21/konata-izumi-dancing-2.html" target="_blank" title="Konata Izumi Dancing 2"><img src="http://cur.cursors-4u.net/cursor.png" border="0" alt="Konata Izumi Dancing 2" style="position:absolute; top: 0px; right: 0px;" /></a>
+<style>
+A:link {text-decoration: none;}
+A:active {text-decoration: none;}
+.auto-style1 {
+	color: #FF0000;
+}
+.auto-style2 {
+	text-align: center;
+}
+.auto-style3 {
+	color: #00FF3C;
+}
+</style>
+</head>
+<body bgcolor="black">
+<div align="center">
+<table width=100% height=100%><td align=center>
+	          <iframe width="550" height="350" src="https://www.youtube.com/embed/cirhQ8iLdbw?rel=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+			<p align="center">
+			<span style="font-weight: 700; margin: 0px; padding: 0px">
+			<font color="#FFFFFF" style="margin: 0px; padding: 0px" face="Consolas" size="6">
+			<font size="5" face="Consolas">Hacked by KosameAmegai</span>
+			<font size="2"><p><i><font color="#ff00e1">do you want to be ignored for the rest of your life or come with me and change the world? 
+
+
+
+
+
+
+
+
+
+
+
+
+
+</i>
+			</span>
+			<p dir="ltr" align="center"><b><font color="#7FFF00">
+			<font size="2" face="Consolas">here's to the crazy one who thinks he can change the world 
+
+
+
+
+			<p>
+			<font size="5"><font color="red">Magical Girl Site<font color=#00ffd0><font size="4"><br></i><font size="3">
+			<a href="http://mahou-shoujo.site" target="_blank" rel="nofollow" title="mahou-shoujo.site">mahou-shoujo.site</a>
+			 
+
+
+
+
+
