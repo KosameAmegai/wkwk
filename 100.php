ecd91ce45830ac5bf4c77e03c27cec1f7c0d08f1
KosameAmegai <84204311+KosameAmegai@users.noreply.github.com>
2022-12-01 15:12:38 +0700

    Add files via upload

---
+++ 100.php
@@ -0,0 +1,59 @@
+<!--------------------------------------------
+Hacked by KosameAmegai
+t.me/kosameamegai, Japan
+zone-h.org/archive/notifier=KosameAmegai
+2k19 - Now
+--------------------------------------------->
+<title>Hacked by KosameAmegai</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta property="og:title" content="Hacked by KosameAmegai">
+<meta name="description" content="merry christmas to u all">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta property="og:description" content="merry christmas to u all">
+<meta property="og:image" content="https://i.ytimg.com/vi/c2Uh0egAjIY/maxresdefault.jpg">
+<link rel="SHORTCUT ICON" href="http://www.pinclipart.com/picdir/big/363-3638329_japanese-flag-clipart.png">
+
+<style>
+            html, body {
+                background-color: #000
+               ;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 100vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 36px;
+                padding: 20px;
+            }
+</style>
+</head>
+<body align="center" oncontextmenu="return false">
+<script type="text/javascript">
+</script>
+<div class="flex-center position-ref full-height">
+	<div class="content">
+		<div class="text">
+			<center>
+			   <iframe width="550" height="350" src="https://www.youtube.com/embed/qn-s5fSkaTw?rel=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+
+			<br>
+			<code>
+			<br>
+			<font color="white" size="5">
+			<b>Hacked by KosameAmegai <br><font color="white" size="4">merry christmas to u
+
+
